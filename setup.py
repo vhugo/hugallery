156766663bd7e7c10ee0abe073edf6d02f4a9283
Victor Alves <victor@sub.pro.br>
2015-12-21 02:56:20 -0200

    Add resources in code

--- setup.py
+++ setup.py
@@ -3,18 +3,12 @@ from setuptools import setup, find_packages
 setup(
 	name='hugallery',
 	version='0.1',
-	description='A static web photo gallery generator for Hugo (http://gohugo.io/)',
+	description='A command line script to generate static photos gallery for Hugo (http://gohugo.io/)',
 	url='http://github.com/vhugo/hugallery',
 	author='Victor Alves',
 	author_email='dev@sub.pro.br',
 	license='MIT',
-	include_package_data=True,
 	install_requires=['Wand'],
 	scripts=["hugallery"],
-	entry_points= {
-		'console_scripts': [
-			'hugallery=hugallery:main',
-		],
-	},
 	zip_safe=False
 )
